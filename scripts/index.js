2a7e786def56ec936525189920903e97b864d2ca
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-06 10:10:33 -0500

    solves Hot Pie exercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,40 @@
-console.log(`There are ${characters.length} characters in the array.`);
+
+// function namesThatStartWithA (listOfCharacters) {
+    //     let count = 0;
+    //     if (characters.indexOf(name, 1));
+    //         count ++;
+    //         console.log(count);
+    // }
+    
+    ///////////////////////////////////////////////////////
+    
+// Hot Pie
+    
+function whoIsHotPie(characters) {
+    console.log(`There are ${characters.length} characters in the array.`);
+    const arrayOfHotPies = characters.filter(function(person) {
+        return person.name === "Hot Pie";
+    });
+    // console.log(arrayOfHotPies);
+    if (arrayOfHotPies.length === 1) {
+        const hotPie = arrayOfHotPies[0];
+        // console.log(hotPie.playedBy);
+        return hotPie.playedBy[0]
+    } else {
+        return "no hot pie";
+    }
+}
+
+function findActorFor(characters, characterName) {
+    const arrayOfHotPies = characters.filter(function(person) {
+        return person.name === characterName;
+    });
+    // console.log(arrayOfHotPies);
+    if (arrayOfHotPies.length === 1) {
+        const hotPie = arrayOfHotPies[0];
+        // console.log(hotPie.playedBy);
+        return hotPie.playedBy[0]
+    } else {
+        return "no hot pie";
+    }
+}
